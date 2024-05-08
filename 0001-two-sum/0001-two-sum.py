c82ee0fcf233e2f90cfa5fdbe9b70c69cc7abbb9
pairi90 <164121806+yumjinmin@users.noreply.github.com>
2024-05-08 13:27:00 +0900

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,52 @@
-# 풀이1 브루트 포스로 계산
+# 4
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
+        for i, num in enumerate(nums):
+            if target-num in nums_map:
+                return [nums_map[target-num], i]
+            nums_map[num] = i
+
+'''
+# 5
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        left, right = 0, len(nums) -1
+        while not left == right:
+            if nums[left] + nums[right] < target:
+                left += 1
+            elif nums[left] + nums[right] > target:
+                right -= 1
+            else:
+                return [left, right]
+
+# 3
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {} # dict
+        for i, num in enumerate(nums):
+            nums_map[num] = i
+        
+        for i, num in enumerate(nums):
+            if target-num in nums_map and i != nums_map[target-num]:
+                return [i, nums_map[target-num]]
+
+# 2
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, n in enumerate(nums):
+            complement = target - n
+
+            if complement in nums[i+1:]:
+                return [nums.index(n), nums[i+1:].index(complement)+(i+1)]
+
+# 1             
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+'''
         
