39c728d93aac93c2ccf9f36abc4ecfd1be67dd55
pairi90 <164121806+yumjinmin@users.noreply.github.com>
2024-05-09 18:49:54 +0900

    Update 0121-best-time-to-buy-and-sell-stock.py

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -5,7 +5,7 @@ class Solution:
         min_price = float('inf') # float
 
         for price in prices:
-            min_price = min(min_price, price)
+            min_price = min(min_price, price) # 비교 오류를 줄이기 위해 사용
             profit = max(profit, price - min_price)
         
         return profit
