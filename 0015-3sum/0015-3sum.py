65041a563f81542ff632962e9b89d5fbdfd254ae
pairi90 <164121806+yumjinmin@users.noreply.github.com>
2024-05-09 18:42:31 +0900

    Update 0015-3sum.py

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i in range(len(nums)-2):
             # 중복된 값 건너뛰기
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1]: # 축에 대한 문제
                 continue
             
             left, right = i+1, len(nums)-1
@@ -18,7 +18,7 @@ class Solution:
                 else:
                     results.append([nums[i], nums[left], nums[right]])
                     # 스킵 처리
-                    while left < right and nums[left] == nums[left+1]:
+                    while left < right and nums[left] == nums[left+1]: 
                         left += 1
                     while left > right and nums[right] == nums[right-1]:
                         right -= 1
