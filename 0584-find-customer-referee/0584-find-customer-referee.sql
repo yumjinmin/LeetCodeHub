cb21dfdc8a97e25c29b934dc323970fc17fa7427
pairi90 <164121806+yumjinmin@users.noreply.github.com>
2024-09-12 23:23:14 +0900

    Update 0584-find-customer-referee.sql

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
 SELECT name FROM Customer WHERE referee_id != 2 or referee_id is null;
 
 # null값은 자동으로 제외되기 때문에 COALESCE(column, replace_value)로 null을 0으로 바꾸어 준 다음 조건을 걸어주면 된다.
