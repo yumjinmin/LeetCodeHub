9e58d949b3a99e0bcd8bd8796bb2e6b7a86e8a18
pairi90 <164121806+yumjinmin@users.noreply.github.com>
2024-09-15 21:50:55 +0900

    Update 2880-select-data.py

--- 2880-select-data/2880-select-data.py
+++ 2880-select-data/2880-select-data.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
     return students[students['student_id'] == 101].iloc[:, 1:]
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.loc[students['student_id']== 101, ['name','age']]
