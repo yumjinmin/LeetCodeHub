1c76280d5f22364ffb71f447ad8a14a31e28abcf
pairi90 <164121806+yumjinmin@users.noreply.github.com>
2024-09-22 15:15:12 +0900

    Update 2886-change-data-type.py

--- 2886-change-data-type/2886-change-data-type.py
+++ 2886-change-data-type/2886-change-data-type.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
     return students.astype({'grade':int})
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students = students.astype({'grade':int}) # 할당 필요함 
+    return students
