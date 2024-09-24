15b5b2c082d7f8c2b46f6f2987d2d75236f9fd3e
pairi90 <164121806+yumjinmin@users.noreply.github.com>
2024-09-24 14:25:56 +0900

    Update 2889-reshape-data-pivot.py

--- 2889-reshape-data-pivot/2889-reshape-data-pivot.py
+++ 2889-reshape-data-pivot/2889-reshape-data-pivot.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
     return weather.pivot(index='month', columns='city', values='temperature')
+
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+    df = pd.pivot(weather, index='month', columns='city', values='temperature')
+    return df
