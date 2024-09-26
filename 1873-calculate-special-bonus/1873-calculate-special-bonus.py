45e4be336a43d56d0eb391f427f136de49d2ce1c
pairi90 <164121806+yumjinmin@users.noreply.github.com>
2024-09-26 11:17:53 +0900

    Time: 493 ms (19.73%), Space: 70.6 MB (51.74%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    # 조건을 만족하는 직원에게 보너스를 지급
+    # if 대신 loc를 사용해 필터링 
+    employees.loc[(employees['employee_id']%2==1) & (~employees['name'].str.startswith('M')), 'bonus'] = employees['salary']
+    # 나머지 직원들에게는 보너스를 0으로 설정
+    employees['bonus'] = employees['bonus'].fillna(0)
+    
+    # employee_id와 bonus만 반환
+    return employees[['employee_id', 'bonus']].sort_values('employee_id')
+    
