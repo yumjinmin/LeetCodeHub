d7cf61b0073904364d5f7f9e0912f94ac4751295
pairi90 <164121806+yumjinmin@users.noreply.github.com>
2024-10-09 20:41:23 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,6 @@
+SELECT query_name, 
+       ROUND(SUM(rating / position) / COUNT(*), 2) AS quality,
+       ROUND((SELECT COUNT(*) FROM Queries WHERE rating < 3 AND query_name = Q.query_name) 
+            / COUNT(*) * 100, 2) AS poor_query_percentage 
+FROM Queries Q
+GROUP BY query_name;
